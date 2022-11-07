f53282938483d5418e9a8280312b1942fd13ac20
Yury Torgashin <imaginarylight@list.ru>
2022-11-08 00:38:45 +0300

    вынести создание стора в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { compose, createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import { rootReducer } from "./services/reducers";
-import thunk from "redux-thunk";
 import "normalize.css";
 import "./index.css";
 import App from "./components/App/App";
-
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
+import { store } from "./services/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
