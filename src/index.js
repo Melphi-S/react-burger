fcf1ecf62276af52e70a482a807e4901c7a63565
Yury Torgashin <imaginarylight@list.ru>
2022-11-30 10:21:03 +0300

    поменять BrowserRouter на HashRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "normalize.css";
 import "./index.css";
 import App from "./components/App/App";
 import { store } from "./services/store";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
